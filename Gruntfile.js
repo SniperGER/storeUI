f5075ba1255a5ed141159ab5b40353a78eba8b6e
SniperGER <janikschmidt@icloud.com>
2018-09-30 23:25:53 +0200

    Added product rating cell Added Watch app variant to small lockup cell

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,7 @@ const lessFiles = [
 	"src/less/cells/product-lockup-cell.less",
 	"src/less/cells/product-media-cell.less",
 	"src/less/cells/product-description-cell.less",
+	"src/less/cells/product-rating-cell.less",
 	
 	"src/less/collection-view.less",
 ];
