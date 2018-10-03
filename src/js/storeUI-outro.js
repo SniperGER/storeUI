8e272b16bc9a6068807f6fd6320b4f70affce5d7
SniperGER <janikschmidt@icloud.com>
2018-10-03 09:43:46 +0200

    Small improvements

--- src/js/storeUI-outro.js
+++ src/js/storeUI-outro.js
@@ -1,14 +1,14 @@
 	}
+	
+	static get touchEventStart() { return ("ontouchstart" in window) ? "touchstart" : "mousedown"; }
+	static get touchEventMove() { return ("ontouchmove" in window) ? "touchmove" : "mousemove"; }
+	static get touchEventEnd() { return ("ontouchend" in window) ? "touchend" : "mouseup"; }
+	static get touchEventClick() { return ("ontouchend" in window) ? "touchend" : "click"; }
+	
+	static get buildVersion() { return "{{buildVersion}}"; }
+	static get buildNumber() { return "{{buildNumber}}"; }
+	static get buildRevision() { return "{{buildRevision}}"; }
+	static get buildBranch() { return "{{buildBranch}}"; }
+	static get buildDate() { return "{{buildDate}}"; }
+	static get buildString() { return "{{buildVersion}}.{{buildNumber}}.{{buildRevision}}_{{buildBranch}}.{{buildDate}}"; }
 }
-
-storeUI.touchEventStart = ("ontouchstart" in window) ? "touchstart" : "mousedown";
-storeUI.touchEventMove = ("ontouchmove" in window) ? "touchmove" : "mousemove";
-storeUI.touchEventEnd = ("ontouchend" in window) ? "touchend" : "mouseup";
-storeUI.touchEventClick = ("ontouchend" in window) ? "touchend" : "click";
-
-storeUI.buildVersion = "{{buildVersion}}";
-storeUI.buildNumber = "{{buildNumber}}";
-storeUI.buildRevision = "{{buildRevision}}";
-storeUI.buildBranch = "{{buildBranch}}";
-storeUI.buildDate = "{{buildDate}}";
-storeUI.buildString = "{{buildVersion}}.{{buildNumber}}.{{buildRevision}}_{{buildBranch}}.{{buildDate}}";
